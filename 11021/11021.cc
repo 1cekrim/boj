0f39745566f098b64ae2a50c0f04db4755120677
1cekrim <1cekrim.dev@gmail.com>
2020-01-08 04:10:35 +0900

    [11021] A+B - 7

---
+++ 11021/11021.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+    int n;
+    std::cin >> n;
+
+    for (int i = 1; i <= n; ++i)
+    {
+        int a, b;
+        std::cin >> a >> b;
+
+        std::cout << "Case #" << i << ": " << a + b << '\n';
+    }
+
+    return 0;
+}
