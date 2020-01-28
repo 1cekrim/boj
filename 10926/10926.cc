e92cb90d78765604f3e17e540e191d64c49c2994
1cekrim <1cekrim.dev@gmail.com>
2020-01-28 23:41:39 +0900

    [10926] ??!

---
+++ 10926/10926.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+    std::string s;
+    std::cin >> s;
+    std::cout << s << "??!\n";
+
+    return 0;
+}
