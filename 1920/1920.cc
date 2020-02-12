a4b391d3ec8dc21845c87bd213ca3963481acb07
1cekrim <1cekrim.dev@gmail.com>
2020-02-12 17:35:17 +0900

    [1920] 수 찾기

---
+++ 1920/1920.cc
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+    int n;
+    std::cin >> n;
+
+    std::vector<int> nums;
+    nums.reserve(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        int a;
+        std::cin >> a;
+        nums.emplace_back(a);
+    }
+
+    std::sort(nums.begin(), nums.end());
+
+    int m;
+    std::cin >> m;
+
+    while (m--)
+    {
+        int a;
+        std::cin >> a;
+
+        std::cout << (*std::lower_bound(nums.begin(), nums.end(), a) == a)
+                  << '\n';
+    }
+
+    return 0;
+}
